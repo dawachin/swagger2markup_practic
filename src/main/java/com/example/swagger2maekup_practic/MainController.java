55583f1567c2a47dc77ed51870fa22630ae06463
hiroaki ohwada <dawachin@git.com>
2017-12-24 21:31:16 +0900

    pathのソートをするクラスを追加。まだうまく動かない

--- src/main/java/com/example/swagger2maekup_practic/MainController.java
+++ src/main/java/com/example/swagger2maekup_practic/MainController.java
@@ -8,7 +8,7 @@ public class MainController {
 
 
     @ApiOperation(value = "最も近いスケジュールを一件返却します。",
-                    notes = "実行された時間より、最も近いスケジュールを一件返却します。但し１年以上先の予定は返却しません。")
+                    notes = "実行された時間より、最も近いスケジュールを一件返却します。但し１年以上先の予定は返却しません。", position = 1)
     @GetMapping(value = "schedule")
     public String getSchedule() {
         return "明日は朝１０時から会議です。";
@@ -16,13 +16,13 @@ public class MainController {
 
     @ApiOperation(value = "指定された日付のスケジュールを返却します。",
     notes = "指定された日付のスケジュールを全件検索し返却します。日を跨いだスケジュールも返却します。" +
-            "** 但し、開始・終了日時が設定されていない予定はTODOとして扱われ返却されます。")
+            "** 但し、開始・終了日時が設定されていない予定はTODOとして扱われ返却されます。", position = 2)
     @GetMapping(value = "schedule/{baseDate}")
     public String getScheduleByDate(@PathVariable String baseDate) {
         return baseDate + "のスケジュールはありません。";
     }
 
-    @ApiOperation(value = "新しいメンバーを検索します。")
+    @ApiOperation(value = "新しいメンバーを追加します。", position = 3)
     @PostMapping(value = "member/add")
     public String postMember(@RequestBody Member member) {
         return member.getName() + "さんの登録を受け付けました。";
--- src/main/java/com/example/swagger2maekup_practic/Member.java
+++ src/main/java/com/example/swagger2maekup_practic/Member.java
@@ -1,28 +1,26 @@
 package com.example.swagger2maekup_practic;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 
 @AllArgsConstructor
+@ApiModel(value = "メンバー", description = "メンバーを管理するBeanです。")
 public class Member {
 
     @Getter
     @ApiModelProperty(value = "名前  " +
-            "メンバーの名前。性と名の間に一文字文の空白を入力してください。")
+            "メンバーの名前。性と名の間に一文字文の空白を入力してください。", position = 2)
     private String name;
 
     @Getter
-    @ApiModelProperty(value = "年齢")
+    @ApiModelProperty(value = "年齢", position = 3)
     private String age;
 
     @Getter
-    @ApiModelProperty(value = "一意のID\n" +
-            "いいいいいいいいいいいいいいい\n" +
-            "いいいいいいいいいいいいいいい\n" +
-            "・ test\n" +
-            "・ test\n")
+    @ApiModelProperty(value = "一意のID", position = 1)
     private String id;
 
 }
